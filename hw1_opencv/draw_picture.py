67e8dc25f93d46a6ac72be29f1cb1736227f84c6
Lo, Yu hsuan <angelalo88362@gmail.com>
2021-03-06 00:12:40 +0800

    picture modified

--- hw1_opencv/draw_picture.py
+++ hw1_opencv/draw_picture.py
@@ -1,17 +1,21 @@
 import numpy as np
 import cv2
 
-img = np.zeros((400, 400, 3), np.uint8)
-img.fill(200)
+img = cv2.imread("./cat.jpg")
+img = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 
-cv2.line(img, (0, 0), (255, 255), (0, 0, 255), 5)
-cv2.rectangle(img, (0, 0), (255, 255), (0, 255, 0), 5)
-cv2.rectangle(img, (0, 0), (100, 100), (0, 255, 255), -5)
-cv2.circle(img, (100, 100), 100, (255, 255, 0), 5)
-cv2.circle(img, (100, 100), 50, (255, 0, 255), -1)
+# img = np.zeros((400, 400, 3), np.uint8)
+# img.fill(200)
 
-cv2.imwrite( "./picture.jpg", img)
+cv2.circle(img, (300, 300), 30, (255, 0, 255), 3, 1)
+cv2.line(img, (300, 70), (480, 220), (0, 255, 0), 5)
+cv2.rectangle(img, (10, 0), (580, 320), (255, 200, 100), 5)
+cv2.rectangle(img, (20, 20), (100, 100), (0, 255, 255), -1)
+cv2.circle(img, (510, 130), 70, (255, 255, 0), 5)
+cv2.putText(img, "I can fly !!!", (80, 400), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 200,
+200), 3)
+cv2.imwrite( "./picture1.jpg", img)
 
 # show pictures
-cv2.imshow( "show_picture", img)
-cv2. waitKey(1000)
+# cv2.imshow( "show_picture", img)
+# cv2. waitKey(1000)
