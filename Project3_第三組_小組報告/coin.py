0022bd5d021d5c4b36229de445ef435d9d9d4ed2
Yu Hsuan <angelalo88362@gmail.com>
2021-03-24 18:05:12 +0800

    P3 1 2 3 done

--- Project3_第三組_小組報告/coin.py
+++ Project3_第三組_小組報告/coin.py
@@ -7,37 +7,38 @@ height = img.shape[0]
 width = img.shape[1]
 # img = cv2.resize(img, (int(width*0.25), int(height*0.25)), interpolation=cv2.INTER_CUBIC)
 # edged = cv2.Canny(img, 30, 150)
-# img = cv2.dilate(img, np.ones((1, 1)), iterations=1)
-# img = cv2.erode(img, np.ones((1, 1)), iterations=1)
+# img = cv2.dilate(img, np.ones((3, 3)), iterations=1)
+# img = cv2.erode(img, np.ones((3, 3)), iterations=1)
+# img = cv2.erode(img, np.ones((3, 3)), iterations=3)
+# img = cv2.dilate(img, np.ones((3, 3)), iterations=4)
+# img = cv2.erode(img, np.ones((3, 3)), iterations=2)
+
 # ret, img  = cv2.threshold(img, 80, 255, cv2.THRESH_BINARY)
 
-
-circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 360, param1=100, param2=30,
- minRadius=180, maxRadius=300)
-
-# print(circles.type())
-print(len(circles[0]))
-#  draw circles
+circles = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 360, param1=100, param2=30,minRadius=180, maxRadius=300)
 if circles is not None:
-    circles = np.uint16(np.around(circles))
-    for i in circles[0, :]:
-        center = (i[0], i[1])
-        # circle center
-        # cv2.circle(img, center, 1, (100, 100, 100), 30)
-        # circle outline
-        radius = i[2]
-        cv2.circle(img, center, radius, (255, 255, 255), -3)
+    print("Total circle detect: " + str(len(circles[0])))
+
+#  draw circles
+# if circles is not None:
+#     circles = np.uint16(np.around(circles))
+#     for i in circles[0, :]:
+#         center = (i[0], i[1])
+#         radius = i[2]
+#         cv2.circle(img, center, radius, (255, 255, 255), -3)
+
+# cv2.imwrite("./coin_out1.jpg", img)
+
 price = 0
 if circles is not None:
     circles = np.uint16(np.around(circles))
     for i in circles[0, :]:
         # center = (i[0], i[1])
-        # circle outline
         # radius = i[2]
         # cv2.circle(img_3, center, radius, (255, 255, 255), -3)
         # print(i[2])
         
-        # rectangle
+        # draw rectangle
         # 1
         if(i[2]<190):
             price += 1
@@ -56,12 +57,12 @@ if circles is not None:
             cv2.rectangle(img_3, (i[0]-i[2], i[1]+i[2]),
             (i[0]+i[2], i[1]-i[2]), (0, 255, 255), 10)
 
-print(price)
+print("Total money is: " + str(price))
 # num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(img, connectivity=8)
 # print(num_labels)
 # blurred = cv2.GaussianBlur(img, (11, 11), 10)
 # ret, img = cv2.threshold(img, 250, 255, cv2.THRESH_BINARY)
 
-img = cv2.resize(img, (int(width*0.25), int(height*0.25)), interpolation=cv2.INTER_CUBIC)
+img_3 = cv2.resize(img_3, (int(width*0.25), int(height*0.25)), interpolation=cv2.INTER_CUBIC)
 
-cv2.imwrite("./coin_out3.jpg", img_3)
+cv2.imwrite("./coin_out.jpg", img_3)
---
+++ Project3_第三組_小組報告/coin2.py
@@ -0,0 +1,81 @@
+import numpy as np
+import cv2
+# coin2
+img_3 = cv2.imread("./coin2.jpg")
+img = cv2.imread("./coin2.jpg", 0)
+img_gray = cv2.imread("./coin2.jpg", 0)
+
+height = img.shape[0]
+width = img.shape[1]
+price = 0
+edged = cv2.Canny(img, 30, 150)
+img = cv2.GaussianBlur(img, (55, 55), 5)
+ret, img  = cv2.threshold(img, 50, 255, cv2.THRESH_BINARY)
+# img = cv2.dilate(img, np.ones((1, 1)), iterations=1)
+# img = cv2.erode(img, np.ones((1, 1)), iterations=1)
+num_labels, labels, stats, centroids = cv2.connectedComponentsWithStats(img, connectivity=8)
+# print(num_labels)
+# print("-------------------------------")
+# print("[    x,   y,    width,    height,    area]")
+# print(stats)
+# print(stats.shape)
+
+for i in stats[:, ]:
+    if(i[4] > 1000000 and i[4] <2000000):
+        x1 = int(i[0])
+        y1 = int(i[1])
+        x2 = int(i[0] + i[2])
+        y2 = int(i[1] + i[3])
+        if(i[4] > 1400000):
+            price += 1000
+            cv2.rectangle(img_3, (x1, y1), (x2, y2), (255, 255, 255), 10)
+        elif(i[2] > 1000):
+            price += 500
+            cv2.rectangle(img_3, (x1, y1), (x2, y2), (255, 0, 255), 10)
+        else:
+            price += 100
+            cv2.rectangle(img_3, (x1, y1), (x2, y2), (255, 0, 0), 10)
+
+# cv2.imwrite("./coin2_out_binary.jpg", img)
+
+
+circles = cv2.HoughCircles(img_gray, cv2.HOUGH_GRADIENT, 1, 250, param1=700, param2=50,minRadius=100, maxRadius=160)
+
+# if circles is not None:
+#     print("Total circle detect: "+ str(len(circles[0])))
+
+#  draw circles
+if circles is not None:
+    circles = np.uint16(np.around(circles))
+    for i in circles[0, :]:
+        center = (i[0], i[1])
+        radius = i[2]
+
+        # draw rectangle
+        # 1
+        if(i[2] < 120):
+            price += 1
+            cv2.rectangle(img_3, (i[0]-i[2], i[1]+i[2]),
+                          (i[0]+i[2], i[1]-i[2]), (0, 0, 255), 10)
+        # 5
+        elif(i[2] < 130):
+            price += 5
+            cv2.rectangle(img_3, (i[0]-i[2], i[1]+i[2]),
+                          (i[0]+i[2], i[1]-i[2]), (0, 125, 255), 10)
+        # 50
+        elif(i[2] > 150):
+            price += 50
+            cv2.rectangle(img_3, (i[0]-i[2], i[1]+i[2]),
+                          (i[0]+i[2], i[1]-i[2]), (0, 255, 0), 10)
+        # 10
+        else:
+            price += 10
+            cv2.rectangle(img_3, (i[0]-i[2], i[1]+i[2]),
+                          (i[0]+i[2], i[1]-i[2]), (0, 255, 255), 10)
+
+print("Total money is: " + str(price))
+
+img_3 = cv2.resize(img_3, (int(width*0.25), int(height*0.25)),
+    interpolation=cv2.INTER_CUBIC)
+
+cv2.imwrite("./coin2_out.jpg", img_3)
