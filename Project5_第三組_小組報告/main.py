f9e783e8d613700ab89168fb7fa85671a0651dc9
YH, Lo <t108820001@ntut.org.tw>
2021-04-13 20:57:08 +0800

    cat

--- Project5_第三組_小組報告/main.py
+++ Project5_第三組_小組報告/main.py
@@ -6,28 +6,40 @@ from sklearn.datasets import fetch_lfw_people
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 import numpy as np
+import cv2
 
 # requirement
 # pip install scikit-datasets
 
 # data preparing
+
 # people
 lfw_people = fetch_lfw_people(min_faces_per_person=100, resize=0.4)
 
-# print(lfw_people.keys())
-# dict_keys(['data', 'images', 'target', 'target_names', 'DESCR'])
+people_x_images = lfw_people.images[0:1000]
+people_y_target = [0 for i in range(0, 1000)]
+people_y_target = np.array(people_y_target)
 
-people_images = lfw_people.images
-people_target = [0 for i in range(0, 1140)]
-people_target = np.array(people_target)
-
-print(people_images.shape)
-print(people_target.shape)
+print(people_x_images.shape)
+print(people_y_target.shape)
 
 # not people
 
-train_folder = r'./train/buildings'
-test_folder = r'./test/buildings'
+cats_train_amount = 1000
+cats_test_amount = 489
+prediction_amount = 10
+
+cats_x_train = []
+for x in range(cats_train_amount):
+    img = cv2.imread('./cats_train_0_999/cat.%d.jpg' % (x))
+    img = cv2.resize(img, (37, 50))
+    cats_x_train.append(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
+cats_x_train = np.array(cats_x_train)
+# print(cats_x_train.shape)
+
+cv2.imshow("test",cats_x_train[990])
+cv2.waitKey(0)
+cats_y_train = [1 for i in range(0, 1000)]
 
 
 # hog
@@ -52,3 +64,7 @@ test_folder = r'./test/buildings'
 
 # clf = svm.SVC(C=1, kernel='linear', gamma='scale')
 # clf.fit(x_train, y_train)
+
+# print("Accuracy")
+# print(clf.score(X_train, y_train))
+# print(clf.score(X_test, y_test))
